1d3c5f5fefa44659044e9737e9543b6bc6da889c
prateekrajgautam <prateekrajgautam@gmail.com>
2022-11-11 10:13:09 +0530

    docker jupyter from python working

--- dockerJupyter/uploadFilestoServer.sh
+++ dockerJupyter/uploadFilestoServer.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-scp Dockerfile requirements.txt script.sh  prateek@10.135.27.6:/home/prateek/jupyterDockerV1/
+scp Dockerfile requirements.txt prateek@10.135.27.6:/home/prateek/sync/
+scp -r ./sync/local2server  prateek@10.135.27.6:/home/prateek/sync/
+scp -r prateek@10.135.27.6:/home/prateek/sync/server2local ./sync/
